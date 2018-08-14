9a73dfe541ff7e7546f5f848679d0d40151f118d
KateNaumenko <rikanaumenko21@gmail.com>
2018-08-14 20:47:46 +0300

    added validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|pgn)\Z}i,
+    message: 'URL повинен вказувати на зображення формата GIF, JPG або PGN.'
+  }
 end
