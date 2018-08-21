b7af05cea24dd25326f4f880d81a1bdb0b9acd47
KateNaumenko <rikanaumenko21@gmail.com>
2018-08-21 16:04:31 +0300

    task E (137-151)

--- app/models/product.rb
+++ app/models/product.rb
@@ -19,9 +19,9 @@ class Product < ActiveRecord::Base
   def ensure_not_referenced_by_any_line_item
     if line_items.empty?
       return true
-      else
-        errors.add(:base, 'существуют товарные позиции')
-        return false
-      end
+    else
+      errors.add(:base, 'существуют товарные позиции')
+      return false
     end
   end
+end
